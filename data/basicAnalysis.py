87dcde0e1208430dfb099af8c79faf200b436e8f
Dhruva Jothik Byrapatna <dhruvajb@Dhruvas-Laptop.local>
2024-07-29 15:29:28 -0500

    Created simple script to import some parameters and test predictive value using linear regression model(part 1a of plan)

--- data/basicAnalysis.py
+++ data/basicAnalysis.py
@@ -5,6 +5,60 @@ from statistics import fmean
 
 pklFiles = ['racingref/pdata','racingref/rdata'  ,'racingref/qdata']
 
+def dataForRace(fileName, posKey, raceNum):
+    raceKey = raceNum*100+24
+    with open(fileName, 'rb') as f:
+        dfRace = pickle.load(f)
+    f.close()
+  
+    #get driver list
+    driverList = dfRace[raceKey]['Driver']
+
+    avgFinishes = pd.DataFrame(columns=["Driver", "Prev", "Prev3", "Prev5", "Prev10", "PrevAll"])
+
+    for driver in driverList:
+        finishes = []
+        #loop thru races in data set
+        for i in range(raceNum-1,0,-1):
+            #get specific dataframe corresponding to race
+            key = i*100+24
+            d = dfRace[key]
+            #print(d)
+            #find row of data corresponding to driver
+            if not d.empty:
+                rowDf = d[d['Driver'] == driver]
+            #if row exists, append finish to finishes
+            if not rowDf.empty:
+                #print(rowDf)
+                finishes.append(rowDf[posKey].tolist()[0])
+        numRaces = len(finishes)
+        #calculate and append the previous finish, the average finish over the last 3, 5, 10, and total season races
+        
+        if numRaces>=10:
+            dfAdd = [driver]
+            #if numRaces>0:
+            dfAdd.append(finishes[0])
+            #if numRaces >=3:
+            prev3 = fmean(finishes[0:3])
+            dfAdd.append(prev3)
+            # else:
+            #     dfAdd.append(-1)
+            #if numRaces >=5:
+            prev5 = fmean(finishes[0:5])
+            dfAdd.append(prev5)
+            # else:
+            #     dfAdd.append(-1)
+            #if numRaces >=10:
+            prev10 = fmean(finishes[0:10])
+            dfAdd.append(prev10)
+            # else:
+            #     dfAdd.append(-1)
+            dfAdd.append(fmean(finishes))
+        #add this to new dataFrame avgFinishes
+            #print(dfAdd)
+            avgFinishes.loc[len(avgFinishes)] = dfAdd
+    return avgFinishes
+
 for fileName in pklFiles:
     #open pickle file
     with open(fileName+".pkl", 'rb') as f:
@@ -59,7 +113,7 @@ for fileName in pklFiles:
     with open(outputFileName, 'wb') as f:
         pickle.dump(avgFinishes, f)
     f.close()
-    print(avgFinishes)
+    #print(avgFinishes)
 
 
     
