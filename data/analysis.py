118ea0260e668357d0a98c4b6fa93ea6dbe1ae95
djbyrapatna <dhruvajb@gmail.com>
2024-07-31 12:03:41 -0500

    Implemented logAnalysis function. Tested logAnalysis function using linear params. Shows some predictive value for predicting top 10's

--- data/analysis.py
+++ data/analysis.py
@@ -1,16 +1,28 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.linear_model import LinearRegression, Ridge, LogisticRegression
 from polyAnalysis import polyRegRun, polyFeatureRanking
 from linAnalysis import linAnalysisRun, linFeatureRanking
+from logAnalysis import logRegRun
 
-model = LinearRegression()
-polyColList = ['Prevrace','Prev10race','Currqual','Currprac','Prev10DRIVERRATINGloop','Prev10AvgPosloop']
-linAnalysisRun("compiledDataX.pkl", "compiledDataY.pkl",model)
+# model = LinearRegression()
+# polyColList = ['Prevrace','Prev10race','Currqual','Currprac','Prev10DRIVERRATINGloop','Prev10AvgPosloop']
+# linAnalysisRun("compiledDataX.pkl", "compiledDataY.pkl",model)
 
-polyRegRun("compiledDataX.pkl", "compiledDataY.pkl",model,polyColList)
+# polyRegRun("compiledDataX.pkl", "compiledDataY.pkl",model,polyColList)
 
-print(linFeatureRanking("compiledDataX.pkl", "compiledDataY.pkl",model))
-print(polyFeatureRanking("compiledDataX.pkl", "compiledDataY.pkl", model, polyColList, features=7))
+# print(linFeatureRanking("compiledDataX.pkl", "compiledDataY.pkl",model))
+# print(polyFeatureRanking("compiledDataX.pkl", "compiledDataY.pkl", model, polyColList, features=7))
 
+model = LogisticRegression()
+cutOffArray = [1,3,5,10,20]
 
+resultArr = logRegRun("compiledDataX.pkl", "compiledDataY.pkl",cutOffArray, metrics=True, probs = True)
+printArr = ['Accuracy', 'Precision', 'Recall', 'F1']
+
+for i in range(len(resultArr)):
+    result = resultArr[i]
+    metricsArray = result[2]
+    print("Metrics for Cutoff Position "+str(cutOffArray[i])+ ":")
+    for j in range(len(metricsArray)):
+        print(printArr[j]+ ": "+ str(metricsArray[j]))
 
---
+++ data/logAnalysis.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from linExport import createTestTrain, importXy
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.preprocessing import LabelEncoder
+
+def logRegSplits(xFile, yFile):
+    X, y = importXy(xFile, yFile)
+    X_train, X_test, y_train, y_test = createTestTrain(X,y, scale = None)
+    return  X_train, X_test, y_train, y_test
+
+def logRegSetup(y_train, y_test, cutoffArray):
+    #X_train, X_test, y_train, y_test = logRegSplits(xFile, yFile)
+    yArr = []
+    for cutoff in cutoffArray:
+        yPair = []
+        for y in (y_train,y_test):
+            logDf = y.copy()
+            logDf.loc[logDf['Pos'] <= cutoff, 'Pos'] = int(1)
+            logDf.loc[logDf['Pos'] > cutoff, 'Pos'] = int(0)
+            logDf['Pos'].astype('int32', copy=False)
+            #y = y.apply(lambda x:1 if x<=cutoff else 0)
+            yPair.append(logDf)
+        yArr.append(yPair)
+    return yArr
+
+def logReg(X_train, X_test, y_train, y_test, model, metrics=False, probs=False):
+    #print(y_train.to_numpy().flatten())
+    label_encoder = LabelEncoder()
+    arr = y_train.to_numpy().flatten()
+    arr = label_encoder.fit_transform(arr)
+    #print(arr)
+    model.fit(X_train, arr)
+
+    # Make predictions
+    y_pred = model.predict(X_test)
+    metArr = []
+    y_prob = None
+
+    testArr = y_test.to_numpy().flatten()
+    testArr = label_encoder.fit_transform(testArr)
+    # Evaluate the model
+    if metrics:
+        accuracy = accuracy_score(testArr, y_pred)
+        precision = precision_score(testArr, y_pred)
+        recall = recall_score(testArr, y_pred)
+        f1 = f1_score(testArr, y_pred)
+        metArr = [accuracy, precision, recall, f1]
+    if probs:
+        y_prob = model.predict_proba(X_test)
+    return [testArr, y_prob, metArr]
+
+def logRegRun(xFile, yFile, cutOffArray, metrics=False, probs=False):
+    X_train, X_test, y_train, y_test = logRegSplits(xFile, yFile)
+    yArr = logRegSetup(y_train, y_test, cutOffArray)
+    #print(len(yArr))
+    retArr = []
+    model = LogisticRegression()
+    for i in range(len(cutOffArray)):
+        ret = logReg(X_train, X_test, yArr[i][0], yArr[i][1], model, metrics=metrics, probs=probs)
+        retArr.append(ret)
+    return retArr
+
+
+
+
+        
