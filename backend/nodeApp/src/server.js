b79159717d9f074b312d95508d4bb084491bec02
djbyrapatna <dhruvajb@gmail.com>
2025-01-08 16:36:27 -0500

    Update server.js to link to frontend port (change for prod)

--- backend/nodeApp/src/server.js
+++ backend/nodeApp/src/server.js
@@ -6,10 +6,12 @@ const modelRoutes = require('./routes/modelRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000' // Specify your frontend's origin
+  }));
 app.use(express.json());
 
 // Routes
